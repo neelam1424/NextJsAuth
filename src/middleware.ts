a8d8fcaf0d4b010e76bc79182151e36e633a9b32
neelam1424 <neelammore1404@gmail.com>
2025-12-22 21:43:21 -0500

    added middleware and me route

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request:NextRequest){
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/' || path === '/login' || path === '/signin'
+
+    //extract token from cookies
+    const token = request.cookies.get('token')?.value || ''
+
+    if(isPublicPath && token){
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if(!isPublicPath && !token){
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+
+export const config ={
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signin',
+    ],
+}
